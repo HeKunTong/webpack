305b33bf14686a94606cc37330ec9eb82e847880
Kuntong <1161709455@qq.com>
2018-05-13 22:52:11 +0800

    antd引入(ps：babel-plugin-import必须引入)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Input } from 'antd';
 import './index.css';
 
 const App = () => (
   <div className='container'>
     <span>这是一个测试！</span>
+    <Input style={{width: '200px'}} defaultValue={12}/>
   </div>
 );
 
